023f6a07f3e464a8b5311e5e51b77f73bc7925e2
Fabio Rib <fabiomatcomp@gmail.com>
2025-03-28 23:36:25 +0000

    Data dispatcher implementation is completed. Technical debt: transform it into a command client

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ The Data Dispatcher is a Golang service responsible for:
 
 The Dockerfile and docker-compose is used in the development scope to test its interaction with
 the project message bus service.
+
+Create a Makefile to automate the commands.
+
+### Kafka Commands - Used during the design process
+Reference of some important kafka commands.********
+
+```shell
+# Creating a new kafka topic
+$ docker-compose exec kafka kafka-topics --create --topic test-topic --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server localhost:9092
+
+# List all topics
+$ docker-compose exec kafka kafka-topics --list --bootstrap-server localhost:9092
+
+# Publish messages in the topic
+$ docker-compose exec kafka kafka-console-producer --topic test-topic --bootstrap-server localhost:9092
+
+# Consuming messages from the topic
+$ docker-compose exec kafka kafka-console-consumer --topic test-topic --from-beginning --bootstrap-server localhost:9092
+```
--- main.go
+++ main.go
@@ -1,5 +1,124 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"github.com/IBM/sarama"
+	"google.golang.org/protobuf/proto"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 
+	pb "github.com/fabioar87/data-dispatcher/proto"
+)
+
+const (
+	csvFilePath = "26027_athlone_s_complete.csv"
+)
+
+func main() {
+	brokers := []string{"localhost:9092", "kafka:9092", "0.0.0.0:9092"}
+	topic := "test-topic"
+
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer(brokers, config)
+	if err != nil {
+		log.Fatal("Error creating Kafka producer: ", err)
+	}
+	defer producer.Close()
+
+	file, err := os.Open(csvFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(bufio.NewReader(file))
+	reader.FieldsPerRecord = -1
+
+	_, err = reader.Read()
+	if err != nil {
+		log.Fatal("Error reading header", err)
+		return
+	}
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if len(record) > 0 && strings.HasPrefix(record[0], "#") {
+			continue
+		}
+
+		if err != nil {
+			log.Println("bad record:", err)
+			continue
+		}
+
+		dayMin, err := ParseFloat(record[1])
+		if err != nil {
+			continue
+		}
+
+		dayMean, err := ParseFloat(record[2])
+		if err != nil {
+			continue
+		}
+
+		dayMax, err := ParseFloat(record[3])
+		if err != nil {
+			continue
+		}
+
+		waterLevel := &pb.WaterLevel{
+			Date:    record[0],
+			DayMin:  dayMin,
+			DayMean: dayMean,
+			DayMax:  dayMax,
+		}
+
+		payload, err := proto.Marshal(waterLevel)
+		if err != nil {
+			log.Printf("Error serialising proto: %v", err)
+			continue
+		}
+
+		msg := &sarama.ProducerMessage{
+			Topic: topic,
+			Value: sarama.ByteEncoder(payload),
+		}
+
+		partition, offset, err := producer.SendMessage(msg)
+		if err != nil {
+			log.Printf("Error sending message to kafka: %v", err)
+		} else {
+			log.Printf("Message sent to partition %d at offset %d", partition, offset)
+		}
+
+		// Simulating the sensor delay
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func ParseFloat(data string) (float32, error) {
+	data_ := strings.TrimSpace(data)
+	if data_ == "" {
+		return 0, errors.New("invalid data")
+	}
+
+	value, err := strconv.ParseFloat(data_, 32)
+	if err != nil {
+		return 0, fmt.Errorf("error parsing value %v", err)
+	}
+
+	return float32(value), nil
 }
